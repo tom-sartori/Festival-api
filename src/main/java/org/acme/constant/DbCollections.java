a2174211bc2d3f00aaeff1a04f753ba97b47b8be
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-03-20 15:30:32 +0100

    Fix merge.

--- src/main/java/org/acme/constant/DbCollections.java
+++ src/main/java/org/acme/constant/DbCollections.java
@@ -4,11 +4,6 @@ public final class DbCollections {
 
 	private DbCollections() { }
 
-	public static final String DATABASE_NAME = "Festival-db";
-	public static final String FRUITS_COLLECTION = "fruits";
 	public static final String USER_COLLECTION = "users";
-	public static final String VOLUNTEERS_COLLECTION = "volunteers";
-	public static final String GAMES_COLLECTION = "games";
-	public static final String ZONES_COLLECTION = "zones";
 	public static final String FESTIVAL_COLLECTION = "festivals";
 }
